76a902dddc9c9b5f1ba618be2eaa18a1e6a4f3c2
Podesta <11512008+Podesta@users.noreply.github.com>
2021-10-19 09:34:08 +0000

    Add middlewares

---
+++ utils/middleware.js
@@ -0,0 +1,46 @@
+const logger = require('./logger');
+const jwt = require('jsonwebtoken');
+
+const unknownEndpoint = (req, res) => {
+  res.status(204).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformed id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: 'validation error' });
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' });
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired' });
+  }
+
+  next(error);
+};
+
+const tokenExtractor = (req, res, next) => {
+  if (req.headers.authorization) {
+    req.token = req.headers.authorization.substring(7);
+  }
+  next();
+};
+
+const userExtractor = (req, res, next) => {
+  if (req.token) {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (decodedToken.id) {
+      req.user = decodedToken.id;
+    }
+  }
+  next();
+};
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+  userExtractor
+};
